2ed69fe1d1ee3fc30ac9886eb01d17bb4ba4f9e0
byeongung <byeongung@capio.ai>
2016-11-01 17:04:56 -0700

    Install nvidia-docker & bridged network

--- examples/vagrant/provision-dev-cluster.sh
+++ examples/vagrant/provision-dev-cluster.sh
@@ -93,6 +93,8 @@ function install_docker_compose {
   pip install docker-compose
   pip install nvidia-docker-compose
   python -m pip install jinja2
+  wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb
+  sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 }
 
 function get_pom_version {
